1418033072cafed6bfe50332b15de581c27e751d
daolmo <67968940+daolmo@users.noreply.github.com>
2020-07-09 15:24:01 -0500

    First commit
    First commit

---
+++ Strings.playground/Pages/Exercise-Making a List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ ## Exercise: Making a List
+ 
+ Lists are great. Here are some constants describing some of the things you’ve learned about strings so far:
+ */
+let constants = "Declaring string constants\n"
+let unicode = "Unicode characters (😎)\n"
+let combining = "Combining strings using +\n"
+let interpolation = "String interpolation (aka Fill in the Blanks)\n"
+let escaping = "Escape characters for \"special powers\"\n"
+let newline = "Making new lines"
+/*:
+ - experiment: Make a new string constant that is a list of the things you’ve learned, with each entry on a new line. Make sure you add the result to the playground page so that you can see the list properly.
+ */
+let result = constants + unicode + combining + interpolation + escaping + newline
+
+
+//:
+//:[Previous](@previous)  |  page 13 of 16  |  [Next: Exercise: A Restaurant](@next)
